9b48c5e96e804b2b526e926902f87bc98e3a777d
Samuel Imanuel Gunawan <samuelimanue@student.unimelb.edu.au>
2024-08-21 13:52:50 +1000

    Added more unit tests

--- src/ecommerce.ts
+++ src/ecommerce.ts
@@ -101,7 +101,9 @@ export function clearCart(): Record<string, number> {
  * @param {number} total - The total cost to apply the discount to.
  * @return {number} The total cost with the discount applied, or the original total cost if the discount is not applicable.
  */
-// export function applyShippingDiscount(total: number): number {
-//   // Activity 1 code here
-//   return 0;
-// }
+ export function applyShippingDiscount(total: number): number {
+  let discounted = total - 10;
+
+   // Activity 1 code here
+   return discounted;
+ }
--- tests/ecommerce.test.ts
+++ tests/ecommerce.test.ts
@@ -1,23 +1,68 @@
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest"; // `expect` is an assertion, `beforeEach` is a for loop thing that is useful to set variable before every unit test
 import {
   addProduct,
   addToCart,
+  removeFromCart,
   calculateTotal,
   clearCart,
+  applyShippingDiscount
 } from "../src/ecommerce";
 
-let cart = {};
-describe("E-commerce System", () => {
+describe("E-commerce System", () => { // Add test to e-commerce system
   beforeEach(() => {
     addProduct("Soap", 100, 10);
     addProduct("Shampoo", 200, 5);
     clearCart();
   });
 
-  it("should calculate price of all products", () => {
+  it("should calculate price of all products", () => { // `it` describes the test? This is the unit test for each func
+    // Arrange
     addToCart("Soap", 2);
     addToCart("Shampoo", 2);
+
+    // Act
     const sum = calculateTotal();
+
+    // Assert
     expect(sum).toBe(600);
+  });
+
+  it("Should add items to cart", () => {
+    // Arrange
+    let cart = {};
+
+    // Act
+    cart = addToCart("Soap", 2);
+
+    // Assert
+    expect(cart["Soap"]).toBe(2);
+  })
+
+  it("Should have enpty cart", () => {
+    // Arrange
+    let cart = {}
+    cart = addToCart("Soap", 2);
+    cart = addToCart("Shampoo", 2);
+
+
+    // Act
+    cart = removeFromCart("Soap", 2);
+    cart = removeFromCart("Shampoo", 2);
+
+
+    // Assert
+    expect(cart).toEqual({});
+  });
+
+  it("Should apply discount to cart", () => {
+    // Arrange
+    let cart = {};
+    cart = addToCart("Soap", 3);
+
+    // Act
+    let discounted = applyShippingDiscount(calculateTotal());
+
+    // Assert
+    expect(discounted).toBe(calculateTotal() - 10);
   });
 });
